52baf0f6f3ba8045b5dfd33864f54b9d33155ad7
Skyler Tolman <skyler237@gmail.com>
2017-03-16 14:16:04 -0600

    Coded prediction stage of GPS smoothing

--- SP8/estimate_states.m
+++ SP8/estimate_states.m
@@ -46,7 +46,8 @@ function xhat = estimate_states(uu, P)
    persistent u_prev
    persistent xhat_att
    persistent xhat_pos
-   persistent P_
+   persistent P_att
+   persistent P_pos
    
    persistent p_prev
    persistent q_prev
@@ -55,15 +56,20 @@ function xhat = estimate_states(uu, P)
    
    if(t == 0)
        % Initialization
+        % Attitude
         xhat_att = zeros(2,1);
         xhat_pos = zeros(7,1);
         u_prev = uu;
-        P_ = diag([(15*pi/180)^2, (15*pi/180)^2]);
+        P_att = diag([(15*pi/180)^2, (15*pi/180)^2]);
         
         p_prev = y_gyro_x;
         q_prev = y_gyro_y;
         r_prev = y_gyro_z;
         Va_prev = sqrt(2/P.rho*y_diff_pres);  
+        
+        % Position
+        pos_covariance = [20, 20, 10, 10*pi/180, 10, 10, 10*pi/180];
+        P_pos = diag(pos_covariance.^2);
    end
         % Check for new measurements
         new_gyro = 0;
@@ -85,105 +91,186 @@ function xhat = estimate_states(uu, P)
            new_gps = 1; 
         end       
         
-        % ====== Attitude estimation ======
-        % u
-        p = y_gyro_x;
-        q = y_gyro_y;
-        r = y_gyro_z;
-        Va = sqrt(2/P.rho*y_diff_pres);        
-        
-        % x
+    % ====== Attitude estimation ======
+    % u
+    p = y_gyro_x;
+    q = y_gyro_y;
+    r = y_gyro_z;
+    Va = sqrt(2/P.rho*y_diff_pres);  
+    
+    phat = LPF(p, p_prev, P.LPF_gyro_alpha);
+    qhat = LPF(q, q_prev, P.LPF_gyro_alpha);
+    rhat = LPF(r, r_prev, P.LPF_gyro_alpha);
+    Vahat = LPF(Va, Va_prev, P.LPF_diff_press_alpha);
+    p_prev = p;
+    q_prev = q;
+    r_prev = r;    
+    Va_prev = Va;
+
+    % x
+    phi = xhat_att(1);
+    theta = xhat_att(2);
+    sp = sin(phi);
+    cp = cos(phi);
+    st = sin(theta);
+    ct = cos(theta);
+    tt = tan(theta);        
+
+    f_att = [p + q*sp*tt + r*cp*tt;...
+           q*cp - r*sp];
+    G = [1, sp*tt, cp*tt, 0;...
+           0, cp,    -sp,   0];        
+
+    % Prediction
+    N = 10;
+    for i=1:N
+        xhat_att = xhat_att + (P.Ts/N)*f_att;   
+
+        % Recompute trig values
         phi = xhat_att(1);
         theta = xhat_att(2);
         sp = sin(phi);
         cp = cos(phi);
         st = sin(theta);
         ct = cos(theta);
-        tt = tan(theta);        
-        
-        f_xu = [p + q*sp*tt + r*cp*tt;...
-               q*cp - r*sp];
+        tt = tan(theta);
+
+        f_att = [p + q*sp*tt + r*cp*tt;...
+                q*cp - r*sp];
         G = [1, sp*tt, cp*tt, 0;...
-               0, cp,    -sp,   0];        
+             0, cp,    -sp,   0];   
+
+        % Jacobians
+        df_att = [q*cp*tt - r*sp*tt,     (q*sp - r*cp)/(ct^2);...
+             -q*sp - r*cp,           0];
+        A = df_att;
+        P_att = P_att + (P.Ts/N)*(A*P_att + P_att*A' + G*P.Q_u*G' + P.Q_att);
+    end
+
+
+    % Correction        
+    h_xu = [q*Va*st + P.gravity*st;...
+           r*Va*ct - p*Va*st - P.gravity*ct*sp;...
+           -q*Va*ct - P.gravity*ct*cp];
+    dh = [0,                 q*Va*ct + P.gravity*ct;...
+         -P.gravity*cp*ct,  -r*Va*st - p*Va*ct + P.gravity*sp*st;...
+         P.gravity*sp*ct,   (q*Va + P.gravity*cp)*st];
+%         C_x = dh(1,:);
+%         L_x = P_att*C_x'/(P.R_accel_x + C_x*P_att*C_x');
+%         P_att = (eye(2) - L_x*C_x)*P_att;
+%         xhat_att = xhat_att + L_x*(y_accel_x - h_xu(1,:));
+%         
+%         C_y = dh(2,:);
+%         L_y = P_att*C_y'/(P.R_accel_y + C_y*P_att*C_y');
+%         P_att = (eye(2) - L_y*C_y)*P_att;
+%         xhat_att = xhat_att + L_y*(y_accel_y - h_xu(2,:));
+%         
+%         C_z = dh(3,:);
+%         L_z = P_att*C_z'/(P.R_accel_z + C_z*P_att*C_z');        
+%         P_att = (eye(2) - L_z*C_z)*P_att;
+%         xhat_att = xhat_att + L_z*(y_accel_z - h_xu(3,:));
+
+    R_att = diag([P.R_accel_x, P.R_accel_y, P.R_accel_z]);
+    y_accel = [y_accel_x; y_accel_y; y_accel_z];
+    C_att = dh;
+    L_att = (1/1000)*P_att*C_att'*inv(R_att + C_att*P_att*C_att'); % HACK   
+    P_att = (eye(2) - L_att*C_att)*P_att;
+    xhat_att = xhat_att + L_att*(y_accel - h_xu);
+    
+    phihat = xhat_att(1);
+    thetahat = xhat_att(2);
+
+    % ====== GPS smoothing ======    
+    if(new_gps)
+        % u
+        Va = Vahat;
+        q = qhat;
+        r = rhat;
+        phi = phihat;
+        theta = thetahat;
+        cp = cos(phi);
+        sp = sin(phi);
+        tp = tan(phi);
+        ct = cos(theta);
+        st = sin(theta); 
+        
+        % xhat
+        pn = xhat_pos(1);
+        pe = xhat_pos(2);
+        Vg = xhat_pos(3);
+        chi = xhat_pos(4);
+        cx = cos(chi);
+        sx = sin(chi);
+        wn = xhat_pos(5);
+        we = xhat_pos(6);
+        psi = xhat_pos(7);
+        cs = cos(psi);
+        ss = sin(psi);        
+        psidot = q*(sp/ct) + r*(cp/ct);
+        
+        f_pos = [Vg*cx;
+                 Vg*sx;
+                 ((Va*cs + wn)*(-Va*psidot*ss) + (Va*ss + we)*(Va*psidot*cs))/Vg;
+                 (P.gravity/Vg)*tp*cos(chi - psi);
+                 0;
+                 0;
+                 psidot];
         
         % Prediction
         N = 10;
         for i=1:N
-            xhat_att = xhat_att + (P.Ts/N)*f_xu;   
-        
-            % Recompute trig values
-            phi = xhat_att(1);
-            theta = xhat_att(2);
-            sp = sin(phi);
-            cp = cos(phi);
-            st = sin(theta);
-            ct = cos(theta);
-            tt = tan(theta);
-            
-            f_xu = [p + q*sp*tt + r*cp*tt;...
-                    q*cp - r*sp];
-            G = [1, sp*tt, cp*tt, 0;...
-                 0, cp,    -sp,   0];   
+            xhat_pos = xhat_pos + (P.Ts_gps/N)*f_pos;
 
-            % Jacobians
-            df = [q*cp*tt - r*sp*tt,     (q*sp - r*cp)/(ct^2);...
-                 -q*sp - r*cp,           0];
-            A = df;
-            P_ = P_ + (P.Ts/N)*(A*P_ + P_*A' + G*P.Q_u*G' + P.Q_att);
+            % Recompute trig values
+            Vg = xhat_pos(3);
+            chi = xhat_pos(4);
+            cx = cos(chi);
+            sx = sin(chi);
+            wn = xhat_pos(5);
+            we = xhat_pos(6);
+            psi = xhat_pos(7);
+            cs = cos(psi);
+            ss = sin(psi);        
+            psidot = q*(sp/ct) + r*(cp/ct);
+            Vgdot = ((Va*cs + wn)*(-Va*psidot*ss) + (Va*ss + we)*(Va*psidot*cs))/Vg;
+
+            f_pos = [Vg*cx;
+                     Vg*sx;
+                     Vgdot;
+                     (P.gravity/Vg)*tp*cos(chi - psi);
+                     0;
+                     0;
+                     psidot];  
+
+            % Jacobian
+            dVgdot_dpsi = (-psidot*Va*(wn*cs + we*ss)/Vg;
+            dchidot_dVg = (-P.gravity/Vg^2)*tp*cos(chi - psi);
+            dchidot_dchi = (-P.gravity/Vg)*tp*sin(chi - psi);
+            dchidot_dpsi = (P.gravity/Vg)*tp*sin(chi - psi);
+            df_pos = [0, 0, cx,         -Vg*sx,         0,              0,              0;
+                      0, 0, sx,         Vg*cx,          0,              0,              0;
+                      0, 0, -Vgdot/Vg,  0,              -psidot*Va*ss,  psidot*Va*cs,   dVgdot_dpsi;
+                      0, 0, dchidot_dVg, dchidot_dchi,  0,              0,              dchidot_dpsi;
+                      0, 0, 0,          0,              0,              0,              0;
+                      0, 0, 0,          0,              0,              0,              0;
+                      0, 0, 0,          0,              0,              0,              0];
+                      
+            A = df_pos;
+            P_pos = P_pos + (P.Ts_gps/N)*(A*P_pos + P_pos*A' + P.Q_pos);
         end
-       
-   
-        % Correction        
-        h_xu = [q*Va*st + P.gravity*st;...
-               r*Va*ct - p*Va*st - P.gravity*ct*sp;...
-               -q*Va*ct - P.gravity*ct*cp];
-        dh = [0,                 q*Va*ct + P.gravity*ct;...
-             -P.gravity*cp*ct,  -r*Va*st - p*Va*ct + P.gravity*sp*st;...
-             P.gravity*sp*ct,   (q*Va + P.gravity*cp)*st];
-%         C_x = dh(1,:);
-%         L_x = P_*C_x'/(P.R_accel_x + C_x*P_*C_x');
-%         P_ = (eye(2) - L_x*C_x)*P_;
-%         xhat_att = xhat_att + L_x*(y_accel_x - h_xu(1,:));
-%         
-%         C_y = dh(2,:);
-%         L_y = P_*C_y'/(P.R_accel_y + C_y*P_*C_y');
-%         P_ = (eye(2) - L_y*C_y)*P_;
-%         xhat_att = xhat_att + L_y*(y_accel_y - h_xu(2,:));
-%         
-%         C_z = dh(3,:);
-%         L_z = P_*C_z'/(P.R_accel_z + C_z*P_*C_z');        
-%         P_ = (eye(2) - L_z*C_z)*P_;
-%         xhat_att = xhat_att + L_z*(y_accel_z - h_xu(3,:));
         
-        R_att = diag([P.R_accel_x, P.R_accel_y, P.R_accel_z]);
-        y_accel = [y_accel_x; y_accel_y; y_accel_z];
-        C_att = dh;
-        L_att = (1/1000)*P_*C_att'*inv(R_att + C_att*P_*C_att'); % HACK   
-        P_ = (eye(2) - L_att*C_att)*P_;
-        xhat_att = xhat_att + L_att*(y_accel - h_xu);
+        % Correction
         
-        % ====== GPS smoothing ======
+    end
+        
+        
 
    
     % Combine estimated states
     pnhat = 0;
     pehat = 0;
-    hhat = 0;
-    Vahat = LPF(Va, Va_prev, P.LPF_diff_press_alpha);
-    Va_prev = Va;
-    
-    phihat = xhat_att(1);
-    thetahat = xhat_att(2);
-    
-    chihat = 0;
-    
-    phat = LPF(p, p_prev, P.LPF_gyro_alpha);
-    qhat = LPF(q, q_prev, P.LPF_gyro_alpha);
-    rhat = LPF(r, r_prev, P.LPF_gyro_alpha);
-    p_prev = p;
-    q_prev = q;
-    r_prev = r;
-    
+    hhat = 0;     
+    chihat = 0;     
     Vghat = 0;
     wnhat = 0;
     wehat = 0;
